4eed6200a0ed81851077732d44ce8b56021699d3
Andrea <massironi.andrea@gmail.com>
2014-01-24 19:58:55 +0100

    up

--- prepare.sh
+++ prepare.sh
@@ -19,11 +19,39 @@ ln -s ../vgTemplate/*root .
 ln -s ../nominals/*root .
 
 
-mkdir electronResolution
-mkdir electronScale_down
-mkdir electronScale_up
+
 mkdir jetEnergyScale_down
 mkdir jetEnergyScale_up
-mkdir metResolution
+
+mkdir electronScale_down
+mkdir electronScale_up
+
 mkdir muonScale_down
 mkdir muonScale_up
+
+mkdir metResolution
+mkdir electronResolution
+
+
+
+cd /home/amassiro/Latinos/CMSSW_5_3_11_patch6/src/HWWAnalysis/ShapeAnalysis/
+eval `scramv1 runtime -sh`
+source test/env.sh
+cd -
+
+
+
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o jetEnergyScale_up/"$1"    -a jetEnergyScale -v up"}'  | /bin/sh
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o jetEnergyScale_down/"$1"  -a jetEnergyScale -v down"}'| /bin/sh
+
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o electronScale_up/"$1"    -a electronScale -v up"}'| /bin/sh
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o electronScale_down/"$1"  -a electronScale -v down"}'| /bin/sh
+
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o muonScale_up/"$1"    -a muonScale -v up"}'| /bin/sh
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o muonScale_down/"$1"  -a muonScale -v down"}'| /bin/sh
+
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o metResolution/"$1"         -a metResolution"}'| /bin/sh
+ls --color=none nominals/ | grep .root | awk '{print "scaleAndSmearTree.py -i nominals/"$1" -o electronResolution/"$1"    -a electronResolution"}'| /bin/sh
+
+
+
