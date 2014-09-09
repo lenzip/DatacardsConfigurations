8d4649ee3feb8a899257829a4b64e955da1a2e91
Andrea <massironi.andrea@gmail.com>
2014-09-09 18:45:34 +0200

    up

---
+++ Hwidth/0jetDF8TeV/shape.py
@@ -0,0 +1,58 @@
+# tag, used to name the intermediate shape files
+tag='mll-mth'
+
+# luminosity to normalize to
+lumi=19.47
+chans=['of_0j']
+
+# dataset to use: Data2012, Data2012A, Data2012B, SI125
+dataset='Data2012'
+
+# set of mc samples: 0j1j, vbf
+mcset='Hwidth'
+
+# variable, or formula to use: mll, mjj, 2*unboostedMr
+# for 2D, use TTree::Draw sytax i.e. x:y
+#variable='mll:mth' # remember, y:x
+variable='mll' # remember, y:x
+
+# selection to apply when
+selection='shape'
+
+# shape range. can be an
+# - hard-coded label
+# - a tuple (nx,xmin,xmax)
+# - 2d tuple (nx,xmin,xmax,ny,ymin,ymax)
+# - 1d array ([x0,..,xn],)
+# - 2d array ([x0,..,xn],[y0,...,ym])
+#range = 'mth-mll-hilospin'
+#range = '(30,80,280,8,0,200)'
+range = (30,0,300)
+
+# statmode: defined the style of the statistical systematics:
+# - unified: 1 up and 1 down histogram, all bins fluctuating up/down respectively
+# - bybin: 2 histograms per bin, where the corresponding bin is fluctuated up/down
+statmode = 'unified'
+
+# label used for the plot's x-axis
+#xlabel='m_{ll} - m_{tH}'
+xlabel='m_{ll}'
+
+# rebin=10
+rebin=1
+
+# directories
+# path_latino: latino's files
+# path_dd: data driven estimates
+# path_bdt: location of bdt-trees
+
+#path_latino = '/home/amassiro/Latinos/Shape/tree_skim_wwmin_2j/'
+path_latino = '/home/amassiro/Latinos/Shape/tree_skim_wwmin/'
+# not to performe datadriven estimation: just comment!
+#path_dd = '/home/amassiro/Latinos/Shape/dd/Shape_ggH2j_2012_20fb/'
+#path_dd = '/home/amassiro/Latinos/Shape/dd/shape_2012_195fb'
+
+# other directories
+path_shape_raw='raw'
+path_shape_merged='merged'
+
