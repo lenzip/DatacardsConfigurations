07297659a645431a2de985fdc3cd7d96d751db98
Andrea <massironi.andrea@gmail.com>
2014-10-16 00:33:13 +0200

    up

---
+++ Hwidth/doDatacardSimple.sh
@@ -0,0 +1,31 @@
+cd /home/amassiro/Latinos/CMSSW_5_3_11_patch6/src/HWWAnalysis/ShapeAnalysis/
+eval `scramv1 runtime -sh`
+source test/env.sh
+cd -
+
+
+
+cd 0jetDF8TeVSimple
+bash doIt.sh
+cd ..
+rm 0jetDF8TeVSimple.tgz
+pack.py -p 0jetDF8TeVSimple
+
+
+cd 1jetDF8TeVSimple
+bash doIt.sh
+cd ..
+rm 1jetDF8TeVSimple.tgz
+pack.py -p 1jetDF8TeVSimple
+
+
+# cd 2jetDF8TeV
+# bash doIt.sh
+# cd ..
+# rm 2jetDF8TeV.tgz
+# pack.py -p 2jetDF8TeV
+
+
+
+
+
