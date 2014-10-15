82e9478d66ab159209ff0132f66d7fe33d888787
Andrea <massironi.andrea@gmail.com>
2014-10-16 00:37:34 +0200

    up

---
+++ Hwidth/0jetDF8TeVSimple/doIt.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+rm -r raw
+rm -r merged
+rm -r datacards
+cd /home/amassiro/Latinos/CMSSW_5_3_11_patch6/src/HWWAnalysis/ShapeAnalysis/
+eval `scramv1 runtime -sh`
+source test/env.sh
+cd -
+
+mkShapes.py -m 125
+
+mkMerged.py -m 125
+
+mkDatacards.py -m 125
+
+
+mkAutopsy.py datacards/hww-19.36fb.mH125.of_0j_shape.txt   --dump=postFit/Hwidth-0j-of-error-signalInjection.root --injectionSignal
+mkAutopsy.py datacards/hww-19.36fb.mH125.of_0j_shape.txt   --dump=postFit/Hwidth-0j-of-error-data.root
+
+
+
+
